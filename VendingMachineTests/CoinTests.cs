ea1b100f699452ac7b16176d0b127799d1f25c1e
Errol Ramirez <eramirez@htec.co.uk>
2018-01-04 17:03:05 -0500

    Added test to identify nickel

---
+++ VendingMachineTests/CoinTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+
+namespace VendingMachineTests
+{
+    public class CoinTests
+    {
+        [Fact]
+        public void WhenNickelIsInsertedTheCoinIsRecognized()
+        {
+            //arrange 
+            CoinManager coinManager = new CoinManager();
+
+            //act
+            CoinType coinType = coinManager.Identify(5.00);
+
+            //assert
+            Assert.Equal(coinType,CoinType.Nickel);
+
+        }
+    }
+}
