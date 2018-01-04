8c4a64049aeb16e6235064a06e74faac31f026cf
Errol Ramirez <eramirez@htec.co.uk>
2018-01-04 17:20:04 -0500

    Added identify method to coin manager

---
+++ VendingMachine/Managers/CoinManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Managers
+{
+    public class CoinManager
+    {
+        public CoinType Identify(double weight)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
