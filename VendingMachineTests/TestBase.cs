ff10953548ae23e576fc967cf61d281ae47c061e
Errol Ramirez <eramirez@htec.co.uk>
2018-01-04 19:15:24 -0500

    Added testbase class for constants

---
+++ VendingMachineTests/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineTests
+{
+    public class TestBase
+    {
+        #region Nickel Constants
+
+        public const double nickelWeight = 5.00;
+        public const double nickelDiameter = 21.21;
+
+        #endregion
+
+        #region Dime Constants
+
+        public const double dimeWeight = 2.268;
+        public const double dimeDiameter = 17.9;
+
+        #endregion
+
+        #region Quarter Constants
+
+        public const double quarterWeight = 5.670;
+        public const double quarterDiameter = 24.26;
+
+        #endregion
+    }
+}
