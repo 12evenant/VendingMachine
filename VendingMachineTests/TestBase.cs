7b4a2f994b83254203f246af097fc626c183d23c
Errol Ramirez <eramirez@htec.co.uk>
2018-01-04 23:11:12 -0500

    Added constant values for each coin type

--- VendingMachineTests/TestBase.cs
+++ VendingMachineTests/TestBase.cs
@@ -12,6 +12,7 @@ namespace VendingMachineTests
 
         public const double nickelWeight = 5.00;
         public const double nickelDiameter = 21.21;
+        public const decimal nickelValue = (decimal)0.05;
 
         #endregion
 
@@ -19,13 +20,14 @@ namespace VendingMachineTests
 
         public const double dimeWeight = 2.268;
         public const double dimeDiameter = 17.9;
-        public const decimal dimeValue = (decimal) 0.05;
+        public const decimal dimeValue = (decimal) 0.10;
         #endregion
 
         #region Quarter Constants
 
         public const double quarterWeight = 5.670;
         public const double quarterDiameter = 24.26;
+        public const decimal quarterValue = (decimal) 0.25;
 
         #endregion
 
@@ -33,7 +35,7 @@ namespace VendingMachineTests
 
         public const double pennyWeight = 3.11;
         public const double pennyDiameter = 19.05;
-
+        public const decimal pennyValue = (decimal) 0.01;
         #endregion
     }
 }
