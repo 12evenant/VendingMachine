93565d697ad36a0e89b535bd4fe94b55129600ca
Errol Ramirez <eramirez@htec.co.uk>
2018-01-04 23:10:37 -0500

    Added Coin generator helper class

---
+++ VendingMachineTests/Helper/CoinGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using VendingMachine;
+
+namespace VendingMachineTests.Helper
+{
+    internal class CoinGenerator : TestBase, IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[] {CoinType.Nickel, nickelWeight, nickelDiameter,nickelValue, true},
+            new object[] {CoinType.Dime, dimeWeight, dimeDiameter, dimeValue,true},
+            new object[] {CoinType.Quarter, quarterWeight, quarterDiameter,quarterValue, true},
+            new object[] { CoinType.Unacceptable, pennyWeight, pennyDiameter, pennyValue,false}
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
