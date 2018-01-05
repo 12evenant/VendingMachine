87fafc5dabe044984b12cc2288fee1c13e9b9a73
Errol Ramirez <eramirez@htec.co.uk>
2018-01-05 02:46:28 -0500

    Added Cola Class

---
+++ VendingMachine/Products/Cola.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Products
+{
+    public class Cola
+    {
+        public decimal Price = (decimal) 1.00;
+
+        public bool Dispense()
+        {
+            return true;
+        }
+    }
+}
