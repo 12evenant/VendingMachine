e21aa45433d6bc299ca642007c9a559964b0f092
Errol Ramirez <eramirez@htec.co.uk>
2018-01-04 19:52:03 -0500

    Added machine class with CurrentValue defaulted to 0, added InsertCoin method to handle current value based on coin inserted

---
+++ VendingMachine/Managers/Machine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Managers
+{
+    public class Machine
+    {
+        protected CoinManager coinManager;
+        public decimal CurrentValue;
+
+        public Machine()
+        {
+            coinManager = new CoinManager();
+            CurrentValue = (decimal)0.0;
+        }
+
+        public void InsertCoin(double weight, double diameter)
+        {
+            CoinType coinType = coinManager.Identify(weight, diameter);
+
+            if (coinType != CoinType.Unacceptable)
+            {
+                switch (coinType)
+                {
+                    case CoinType.Dime:
+                        CurrentValue = CurrentValue + (decimal) 0.10;
+                        break;
+                }
+            }
+        }
+    }
+}
