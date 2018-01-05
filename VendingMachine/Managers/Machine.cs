4e2dc847cac518bdafd2770e09c7f276a889a654
Errol Ramirez <eramirez@htec.co.uk>
2018-01-04 20:01:09 -0500

    Refactor InsertCoin move adding of value into another method

--- VendingMachine/Managers/Machine.cs
+++ VendingMachine/Managers/Machine.cs
@@ -9,24 +9,30 @@ namespace VendingMachine.Managers
     public class Machine
     {
         protected CoinManager coinManager;
+
         public decimal CurrentValue;
 
         public Machine()
         {
             coinManager = new CoinManager();
-            CurrentValue = (decimal)0.0;
+            CurrentValue = (decimal) 0.0;
         }
 
         public void InsertCoin(double weight, double diameter)
         {
             CoinType coinType = coinManager.Identify(weight, diameter);
 
+            AddOrIgnoreCoinToValue(coinType);
+        }
+
+        private void AddOrIgnoreCoinToValue(CoinType coinType)
+        {
             if (coinType != CoinType.Unacceptable)
             {
                 switch (coinType)
                 {
                     case CoinType.Dime:
-                        CurrentValue = CurrentValue + (decimal) 0.10;
+                        CurrentValue = CurrentValue + (decimal)0.10;
                         break;
                 }
             }
