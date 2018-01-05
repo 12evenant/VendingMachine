c5b723e4544de7c5293f645f0a73b5f5a5ba0abe
Errol Ramirez <eramirez@htec.co.uk>
2018-01-05 17:35:15 -0500

    Added helper product selector class

---
+++ VendingMachineTests/Helper/ProductSelector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.Enums;
+
+namespace VendingMachineTests.Helper
+{
+    internal class ProductSelector : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _productTypes = new List<object[]>
+        {
+            new object[] { ProductType.Cola },
+            new object[] { ProductType.Candy},
+            new object[] { ProductType.Chips}
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _productTypes.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
