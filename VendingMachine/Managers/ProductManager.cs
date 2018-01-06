34ab356a071d6ba4403e1122f0976057561f5995
Errol Ramirez <eramirez@htec.co.uk>
2018-01-05 22:39:47 -0500

    Update product manager to handle logic for getting product details, removing stock and dispensing stock based on product type passed

--- VendingMachine/Managers/ProductManager.cs
+++ VendingMachine/Managers/ProductManager.cs
@@ -14,6 +14,7 @@ namespace VendingMachine.Managers
         protected Chips chips;
         protected Candy candy;
         protected SelectedProduct SelectedProduct;
+
         public ProductManager()
         {
             cola = new Cola();
@@ -23,9 +24,75 @@ namespace VendingMachine.Managers
 
         public SelectedProduct GetSelectedProductDetails(ProductType productType)
         {
-            SelectedProduct
+            switch (productType)
+            {
+                case ProductType.Cola:
+                {
+                    SelectedProduct.Price = cola.GetPrice();
+                    SelectedProduct.Stock = cola.GetStock();
+                    return SelectedProduct;
+                }
+                case ProductType.Chips:
+                {
+                    SelectedProduct.Price = chips.GetPrice();
+                    SelectedProduct.Stock = chips.GetStock();
+                    return SelectedProduct;
+                }
+                case ProductType.Candy:
+                {
+                    SelectedProduct.Price = candy.GetPrice();
+                    SelectedProduct.Stock = candy.GetStock();
+                    return SelectedProduct;
+                }
+                default:
+                    return null;
+            }
         }
 
+        public void RemoveStock(ProductType productType)
+        {
+            switch (productType)
+            {
+                case ProductType.Cola:
+                {
+                    cola.RemoveStock();
+                    break;
+                }
+                case ProductType.Chips:
+                {
+                    chips.RemoveStock();
+                    break;
+                }
+                case ProductType.Candy:
+                {
+                    candy.RemoveStock();
+                    break;
+                }
+            }
+        }
 
+        public bool Dispense(ProductType productType)
+        {
+            switch (productType)
+            {
+                case ProductType.Cola:
+                {
+                    bool colaDispensed = cola.Dispense();
+                    return colaDispensed;
+                }
+                case ProductType.Chips:
+                {
+                    bool chipsDispensed = chips.Dispense();
+                    return chipsDispensed;
+                }
+                case ProductType.Candy:
+                {
+                    bool candyDispensed = candy.Dispense();
+                    return candyDispensed;
+                }
+                default:
+                    return false;
+            }
+        }
     }
 }
