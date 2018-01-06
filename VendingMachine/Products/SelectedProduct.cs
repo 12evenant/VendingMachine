6d51af5d6f55eb780068dbeae599ac85d30a6693
Errol Ramirez <eramirez@htec.co.uk>
2018-01-05 22:16:19 -0500

    Added selected product class

---
+++ VendingMachine/Products/SelectedProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Products
+{
+    public class SelectedProduct
+    {
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
